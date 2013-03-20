9fe8cb67d3c3d7646663cebb9e702411541d041b
madizza <madizza.colico@reloadmedia.com.au>
2013-03-20 15:41:34 -0700

    i need space jerry

---
+++ Izza.php
@@ -0,0 +1,20 @@
+<html>
+<title>Izzalou</title>
+
+<body>
+
+<?php  
+  
+class MyClass  
+{  
+    public $prop1 = "Izza here";  
+}  
+  
+$obj = new MyClass;  
+  
+echo $obj->prop1; // Output the property  
+  
+?>
+
+</body>
+</html>
