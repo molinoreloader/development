fb9b2a3d588a28a033299ccd26665124ed17dca0
Jerry Beniten <jerry.beniten@reloadmedia.com.au>
2013-03-20 10:39:24 -0700

    [#46470465] - The Feature is done

---
+++ operations.class.php
@@ -0,0 +1,29 @@
+<?php
+
+//two variables
+abstract class OperationOne
+{
+        abstract public function multiplication();
+        abstract public function division();
+}
+
+//three variables
+abstract class OperationTwo
+{
+        abstract public function division();
+        abstract public function addition();
+}
+
+//four variables
+abstract class OperationThree
+{
+        abstract public function addition();
+        abstract public function subtraction();
+}
+
+//five variables
+abstract class OperationFour
+{
+        abstract public function multiplication();
+        abstract public function subtraction();
+}
